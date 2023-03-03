038cbd63db8ca3c12862862c0c8038c6ef0ba4e7
Prof Edir <ecaramba@gmail.com>
2023-03-03 20:25:52 -0300

    imprime dom

--- exercicios.js
+++ exercicios.js
@@ -8,13 +8,18 @@ function imprimir()
     var div = document.getElementById("resposta");
     console.log(div);
 
+    // limpa o div
+    div.innerHTML = "";
+
     var ini = 0;
     var fim = cidades.length;
 
     while (ini < fim)
     {
-        div.innerHTML = cidades[ini]
-        ini = ini + 1;
+        div.innerHTML += cidades[ini] + "<br>";
+        // div.innerHTML = div.innerHTML + cidades[ini];
+        ini += 1;
+        // ini = ini + 1;
     }
 
 
