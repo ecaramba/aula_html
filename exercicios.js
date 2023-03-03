557575e339888cca1df13935049a4bba08dab370
Prof Edir <ecaramba@gmail.com>
2023-03-03 20:49:58 -0300

    ordenado

--- exercicios.js
+++ exercicios.js
@@ -5,7 +5,7 @@ var cidades = ["Curitiba", "Pinhais", "Araucaria", "Colombo"];
 function imprimir()
 {
 
-    var div = document.getElementById("resposta");
+    
     console.log(div);
 
     // limpa o div
@@ -22,5 +22,20 @@ function imprimir()
         // ini = ini + 1;
     }
 
+
+}
+
+function ordenado()
+{
+    var div = document.getElementById("resposta");
+    div.innerHTML = "";
+
+    
+    var fim = cidades.length;
+
+    for (var ini = 0; ini < fim; ini += 1)
+    {
+        div.innerHTML += cidades[ini] + "<br>";
+    }
 
 }
