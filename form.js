d7b29f0e692ede7e3b371a2d59679bb9f831624b
Prof Edir <ecaramba@gmail.com>
2023-03-10 20:52:43 -0300

    plugins

--- form.js
+++ form.js
@@ -1,60 +1,72 @@
 
 $(document).ready(function(){
 
+    // plugin calendario
+    $("#data").datepicker({
+        format: "dd/mm/yyyy",
+        endDate: "now",
+        language: "pt-BR"
+    }); //plugin
+
+    // mascara no telefone
+    $("#telefone").mask("(00) 0000-0000");
+
+    // monitora teclado
+    $("#cpf").keydown(function(ev)
+    {
+        console.log(ev.key);
+        var teclas = "1234567890.-";
+
+        if (teclas.includes(ev.key) == false)
+        {
+            return false;
+        }
+       
+    });
+
     // clica no botao cadastrar
     $("#btn-cadastrar").click(function()
     {
+
+        // reseta todos os inputs
+        $("input").removeClass("is-invalid");
 
         var email = $("#email").val();
 
         if (email == "")
         {
             $("#email").addClass("is-invalid");
-        } else {
-            $("#email").removeClass("is-invalid");
         }
 
         if ( $("#nome").val() == "" )
         {
             $("#nome").addClass("is-invalid");
-        } else {
-            $("#nome").removeClass("is-invalid");
         }
 
         if ( $("#cpf").val() == "" )
         {
             $("#cpf").addClass("is-invalid");
-        } else {
-            $("#cpf").removeClass("is-invalid");
-        }
+        } 
 
         if ( $("#telefone").val() == "" )
         {
             $("#telefone").addClass("is-invalid");
-        } else {
-            $("#telefone").removeClass("is-invalid");
         }
 
         if ( $("#cidade").val() == "0" )
         {
             $("#cidade").addClass("is-invalid");
-        } else {
-            $("#cidade").removeClass("is-invalid");
-        }
+        } 
 
         if ( $("[name=sexo]:checked").val() == undefined )
         {
             $("[name=sexo]").addClass("is-invalid");
-        } else {
-            $("[name=sexo]").removeClass("is-invalid");
         }
 
         if ( $("#data").val() == "" )
         {
             $("#data").addClass("is-invalid");
-        } else {
-            $("#data").removeClass("is-invalid");
-        }
+        } 
 
 
     }); // fim do click casdatrar
