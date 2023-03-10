637829ae12187680113a487571503af8a2abe15c
Prof Edir <ecaramba@gmail.com>
2023-03-09 21:49:42 -0300

    validacao ok

--- form.js
+++ form.js
@@ -14,6 +14,48 @@ $(document).ready(function(){
             $("#email").removeClass("is-invalid");
         }
 
+        if ( $("#nome").val() == "" )
+        {
+            $("#nome").addClass("is-invalid");
+        } else {
+            $("#nome").removeClass("is-invalid");
+        }
+
+        if ( $("#cpf").val() == "" )
+        {
+            $("#cpf").addClass("is-invalid");
+        } else {
+            $("#cpf").removeClass("is-invalid");
+        }
+
+        if ( $("#telefone").val() == "" )
+        {
+            $("#telefone").addClass("is-invalid");
+        } else {
+            $("#telefone").removeClass("is-invalid");
+        }
+
+        if ( $("#cidade").val() == "0" )
+        {
+            $("#cidade").addClass("is-invalid");
+        } else {
+            $("#cidade").removeClass("is-invalid");
+        }
+
+        if ( $("[name=sexo]:checked").val() == undefined )
+        {
+            $("[name=sexo]").addClass("is-invalid");
+        } else {
+            $("[name=sexo]").removeClass("is-invalid");
+        }
+
+        if ( $("#data").val() == "" )
+        {
+            $("#data").addClass("is-invalid");
+        } else {
+            $("#data").removeClass("is-invalid");
+        }
+
 
     }); // fim do click casdatrar
 
